ead3ae81e4c22261992c91bbe38d716d255e6966
ryohei.yamamoto <h031203yama@gmail.com>
2021-12-09 23:51:12 +0900

    hands-on: add hands-on comment

--- src/searcher/searcher.js
+++ src/searcher/searcher.js
@@ -34,21 +34,24 @@ class Searcher {
   async search(query, limit = 10, andSearch = false, sortByToken = false) {
     const tokens = this.analyzer.analyze(query);
 
-    // FIXME: 分割したトークンからsurface取り出し、ストレージからインデックスを取得する. surfaceの重複を排除しておいた方がIOが減るので効率的.
-    // TIPS: toSet(array) というutil関数を使うとstring配列から重複を排除した配列を取得できる
+    // FIXME: 分割したトークンからindexId(token.surface)を取り出し、ストレージからインデックスを取得する
+    // TIPS:
+    //   - indexIdの重複を排除しておいた方がIOが減るので効率的
+    //   - toSet(array) というutil関数を使うとstring配列から重複を排除した配列を取得できる
 
-    // FIXME: 取得したインデックスから文書IDを取り出し、ストレージから文書を取得する. 事前に文書IDの重複を排除しておかないと同じ文書が複数取れてしまうかも...
-    // FIXME: 取得した文書をSearchResultに詰める
+    // FIXME: 取得したインデックスから文書IDを取り出し、ストレージから文書を取得する. 取得した文書はDocumentResult(matchedTokenUseCountは一旦0でOK)に詰め替えてからSearchResultに詰める
+    // TIPS: 事前に文書IDの重複を排除しておかないと同じ文書が複数取れてしまうかも...
+
     // FIXME: limitで指定された数だけ文書をストレージから取得するようにする
 
+    return new SearchResult([], 0);
     // ADVANCED: andSearch = true のとき、全てのトークンを含んでいる文書のみ検索にヒットするようにしてみよう
     // TIPS: 取得できたIndexごとに含まれる文書IDの積集合を取ると全てのトークンを含んでいる文書IDが特定できるよ
 
     // ADVANCED: sortByToken = true のとき、トークンの利用回数が多い文書を先頭に並び替えてみよう
-    // TIPS: まずは文書ごとのトークン利用回数を集計して key: 文書ID, value: keyの文書に登場したトークン利用回数の合計値 というMapを作ってみよう
-    // TIPS: Array.from({トークン利用回数を集計したMap}.entries()) とすると [文書ID, トークン利用回数の合計値][] という二次元配列ができるのでsortすることができるよ
-
-    return new SearchResult([], 0);
+    // TIPS:
+    //   - まずは文書ごとのトークン利用回数を集計して key: 文書ID, value: keyの文書に登場したトークン利用回数の合計値 というMapを作ってみよう
+    //   - Array.from({トークン利用回数を集計したMap}.entries()) とすると [文書ID, トークン利用回数の合計値][] という二次元配列ができるのでsortすることができるよ
   }
 }
 
